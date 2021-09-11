b1ad7b479847ef7aea9a31596abd7f4eb804d4e9
MakeItEasyDev <89842411+MakeItEasyDev@users.noreply.github.com>
2021-09-11 19:01:35 +0530

    Add files via upload

---
+++ app/src/main/java/com/jetpack/splashscreenanimation/MainActivity.kt
@@ -0,0 +1,81 @@
+package com.jetpack.splashscreenanimation
+
+import android.os.Bundle
+import android.view.animation.OvershootInterpolator
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.jetpack.splashscreenanimation.ui.theme.SplashScreenAnimationTheme
+import kotlinx.coroutines.delay
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            SplashScreenAnimationTheme {
+                SplashScreenAnimate()
+            }
+        }
+    }
+}
+
+@Composable
+fun SplashScreenAnimate() {
+
+    val scale = remember { Animatable(0f) }
+
+    LaunchedEffect(key1 = true) {
+        scale.animateTo(
+            targetValue = 0.5f,
+            animationSpec = tween(
+                durationMillis = 500,
+                easing = {
+                    OvershootInterpolator(2f).getInterpolation(it)
+                }
+            )
+        )
+
+        delay(3000L)
+    }
+
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.jetpack_compose),
+            contentDescription = "Splash Image",
+            modifier = Modifier
+                .width(300.dp)
+                .height(300.dp)
+                .scale(scale.value)
+        )
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
